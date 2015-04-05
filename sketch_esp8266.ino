3cc598d662b2af8529d3961373c16b190fe330cb
Eugene Mamin <TheDZhon@gmail.com>
2015-04-05 20:47:03 +0300

    Sources restrucured and cleaned.
    Notes moved to separate header with defines.
    Many magic numbers moved to constants or defines.
    Functions naming fixed for consistency.

--- sketch_esp8266.ino
+++ sketch_esp8266.ino
@@ -1,109 +1,31 @@
+#include "tones.h"
+
 #include <DHT22_430.h>
 
-char in_buf[1024];
-char send_buf[256];
-char dht_buf[256];
+#define DHT_PIN PD_0
+#define ESP8266_CH_PD_PIN PB_5
+#define BUZZER_PIN PB_4
+#define RED_LED PF_1
 
-DHT22 dht (PD_0);
+const unsigned kMaxRcvBufSz = 1024U;
+const unsigned kMaxSndBufSz = 256U;
+const unsigned kMaxDHTBufSz = 256U;
+const unsigned kMeasurementsDelayMs = 10000U;
+const unsigned kAlarmDelayMs = 1000U;
+const unsigned kMaxRetryCnt = 100U;
 
-const unsigned max_retry_cnt = 100U;
+char in_buf[kMaxRcvBufSz];
+char send_buf[kMaxSndBufSz];
+char dht_buf[kMaxDHTBufSz];
 
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
+DHT22 dht (DHT_PIN);
 
 void setup()
 {
-  pinMode (PB_5, OUTPUT);
-  pinMode (PB_4, OUTPUT);
+  pinMode (ESP8266_CH_PD_PIN, OUTPUT);
+  pinMode (BUZZER_PIN, OUTPUT);
 
-  play_init_tone ();
+  playInitTone ();
 
   Serial1.begin (115200);
   Serial.begin (115200);
@@ -137,19 +59,19 @@ void loop()
     esp8266send (dht_buf); 
   }
 
-  delay (10000);
+  delay (kMeasurementsDelayMs);
 }
 
 void sTone (unsigned note, unsigned len)
 {
   unsigned duration = 1000U / len;
 
-  tone (PB_4, note, duration);
+  tone (BUZZER_PIN, note, duration);
   delay (duration * 1.3);
-  noTone (PB_4);
+  noTone (BUZZER_PIN);
 }
 
-void play_init_tone ()
+void playInitTone ()
 {
   sTone (NOTE_E4, 8);
   sTone (NOTE_GS4, 4);
@@ -157,7 +79,7 @@ void play_init_tone ()
   sTone (NOTE_E5, 2);
 }
 
-void play_failure_tone ()
+void playFailureTone ()
 {
   sTone (NOTE_B3, 8);
   sTone (NOTE_A4, 4);
@@ -171,14 +93,14 @@ void play_failure_tone ()
 void esp8266shutdown ()
 {
   Serial.println ("Shutdown ESP8266...");
-  digitalWrite (PB_5, LOW);
+  digitalWrite (ESP8266_CH_PD_PIN, LOW);
   delay (5000);
 }
 
 void esp8266poweron ()
 {
   Serial.println ("Power on ESP8266...");
-  digitalWrite (PB_5, HIGH);
+  digitalWrite (ESP8266_CH_PD_PIN, HIGH);
 
   Serial.println ("Clean ESP8266 boot buffer...");
   delay (5000);
@@ -205,7 +127,7 @@ void esp8266waitrx (const char * cmd) {
     ++retry_cnt;
     delay (100); 
 
-    if (retry_cnt > max_retry_cnt) { 
+    if (retry_cnt > kMaxRetryCnt) { 
       failure (cmd);
     }
   }
@@ -253,7 +175,7 @@ void esp8266send (const char * packet)
 
 void failure (const char * cmd)
 {  
-  pinMode (PF_1, OUTPUT);
+  pinMode (RED_LED, OUTPUT);
 
   unsigned cnt = 0;
   unsigned tone_interval = 1U;
@@ -267,11 +189,11 @@ void failure (const char * cmd)
       Serial.println ("[NO_CMD]");
     }
 
-    digitalWrite (PF_1, (cnt % 2) ? HIGH : LOW);
-    delay (1000);
+    digitalWrite (RED_LED, (cnt % 2) ? HIGH : LOW);
+    delay (kAlarmDelayMs);
 
     if ((cnt % tone_interval) == 0) {
-      play_failure_tone ();
+      playFailureTone ();
       tone_interval *= 10U;
     }
 
